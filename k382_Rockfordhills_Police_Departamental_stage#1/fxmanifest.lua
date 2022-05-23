83a0a16911821dc18942859735a16fff4948c6c2
KEIHAAN382 <85694100+Keihaan382@users.noreply.github.com>
2022-05-23 19:23:59 -0300

    Add files via upload

---
+++ k382_Rockfordhills_Police_Departamental_stage#1/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+this_is_a_map 'yes'
+
+author 'Keihaan382 <keihaan382#0977>'
+description 'Rockford Hills Police Departament Fase #1'
+description2 'www.instagram.com/keihaan382'
+description3 'www.twitch.tv/keihaan382'
+
+version ' Freeversion fase #1 v1.0.0'
